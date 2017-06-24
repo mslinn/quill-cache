a3d0642023259c45e77017558bc4c2f97d0c2ba6
Mike Slinn <mslinn@micronauticsresearch.com>
2017-06-23 17:53:03 -0700

    Eh

--- src/main/scala/model/persistence/QuillConfiguration.scala
+++ src/main/scala/model/persistence/QuillConfiguration.scala
@@ -7,6 +7,9 @@ import scala.reflect.runtime.universe._
 
 protected sealed class DbWitness[T: TypeTag](val ctx: T)
 class H2Witness[N](configPrefix: String)       extends DbWitness(new H2JdbcContext[N](configPrefix))
+/* Error:(9, 56) No TypeTag available for io.getquill.H2JdbcContext[N]
+   Error:(9, 56) not enough arguments for constructor DbWitness: (implicit evidence$1: reflect.runtime.universe.TypeTag[io.getquill.H2JdbcContext[N]])model.persistence.DbWitness[io.getquill.H2JdbcContext[N]].
+  Unspecified value parameter evidence$1. */
 class MysqlWitness[N](configPrefix: String)    extends DbWitness(new MysqlJdbcContext[N](configPrefix))
 class PostgresWitness[N](configPrefix: String) extends DbWitness(new PostgresJdbcContext[N](configPrefix))
 class SqliteWitness[N](configPrefix: String)   extends DbWitness(new SqliteJdbcContext[N](configPrefix))
@@ -20,6 +23,7 @@ object QuillConfiguration {
 
   protected lazy val dbType: String = config.getString("use")
 
+  // What is the return type of dbWitness?
   def dbWitness[N <: NamingStrategy] = dbType match {
     case "h2"       => new H2Witness[N](s"persistence-config.$dbType")
     case "mysql"    => new MysqlWitness[N](s"persistence-config.$dbType")
