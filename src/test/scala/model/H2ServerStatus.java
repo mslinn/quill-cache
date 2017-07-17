054edb4ff80491344e45313907c56c3e84eb08ba
Mike Slinn <mslinn@micronauticsresearch.com>
2017-07-17 13:41:14 -0700

    -

---
+++ src/test/scala/model/H2ServerStatus.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum H2ServerStatus {
+    DOES_NOT_EXIST, CREATED, RUNNING
+}
--- src/test/scala/model/SetupTeardown.scala
+++ src/test/scala/model/SetupTeardown.scala
@@ -1,35 +1,79 @@
 package model
 
+import H2ServerStatus._
+import LocalH2Server.h2ServerStatus
 import model.dao.SelectedCtx
 import model.persistence._
 import org.h2.tools.Server
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
 
-abstract class TestSpec extends WordSpec with Matchers with LocalH2Server with SelectedCtx with QuillImplicits with SetupTeardown
+
+abstract class TestSpec
+  extends WordSpec
+    with Matchers
+    with LocalH2Server
+    with SelectedCtx
+    with QuillImplicits
+    with SetupTeardown
+
+object LocalH2Server {
+  var h2ServerStatus: H2ServerStatus = DOES_NOT_EXIST
+}
 
 trait LocalH2Server {
-  val h2Server: Server = org.h2.tools.Server.createTcpServer()
-  h2Server.start()
+  import LocalH2Server._
+
+  val h2Server: Server = if (h2ServerStatus == DOES_NOT_EXIST) {
+    logger.warn("Creating H2 TCP server")
+    val h2: Server = org.h2.tools.Server.createTcpServer()
+    h2ServerStatus = CREATED
+    h2
+  } else h2Server
+
+  if (h2ServerStatus == CREATED) {
+    logger.warn("Starting H2 TCP server")
+    h2Server.start()
+    h2ServerStatus = RUNNING
+  }
 }
 
 // Because BeforeAndAfterAll invokes super.run, mix this trait in last
-trait SetupTeardown extends BeforeAndAfterAll { this: LocalH2Server =>
+trait SetupTeardown extends BeforeAndAfterAll { this: WordSpec with LocalH2Server =>
   val resourcePath = "evolutions/default/1.sql" // for accessing evolution file as a resource from a jar
   val fallbackPath = s"src/test/resources/$resourcePath" // for testing this project
   val processEvolution = new ProcessEvolution(resourcePath, fallbackPath)
 
   override def beforeAll(): Unit = {
-    try { h2Server.start() } catch { case _: Throwable => }
+    assert (h2ServerStatus == CREATED || h2ServerStatus == RUNNING)
 
-    logger.warn(s"Creating H2 embedded database from $resourcePath or $fallbackPath")
-    try { processEvolution.downs(SelectedCtx) } catch { case _: Throwable => } // just in case the last session did not clean up
+    if (h2ServerStatus == CREATED) try {
+      logger.warn("Starting H2 TCP server")
+      h2Server.start()
+      h2ServerStatus = RUNNING
+    } catch { case e: Throwable =>
+      logger.warn(s"Error starting H2 TCP server: ${ e.getMessage }")
+    }
+
+    try { // In case the last session did not clean up
+      logger.warn(s"Creating H2 database from $resourcePath or $fallbackPath")
+      processEvolution.downs(SelectedCtx)
+    } catch { case e: Throwable =>
+      logger.warn(e.getMessage)
+    }
     processEvolution.ups(SelectedCtx)
     logger.warn("H2 embedded database should exist now.")
   }
 
   override def afterAll(): Unit = {
-    processEvolution.downs(SelectedCtx)
+    super.afterAll()
 
-    h2Server.stop()
+    if (h2ServerStatus == RUNNING) {
+      logger.warn(s"Deleting H2 database")
+      processEvolution.downs(SelectedCtx)
+
+      logger.warn("Stopping H2 TCP server")
+      h2Server.stop()
+      h2ServerStatus = CREATED
+    }
   }
 }
