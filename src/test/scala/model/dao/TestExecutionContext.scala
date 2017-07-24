11e922b4b0008d7df16c4f47e5d4b1edb4562e59
Mike Slinn <mslinn@micronauticsresearch.com>
2017-07-24 09:24:28 -0700

    -

--- src/test/scala/model/dao/TestExecutionContext.scala
+++ src/test/scala/model/dao/TestExecutionContext.scala
@@ -5,7 +5,7 @@ import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
 
 /** Just delegates to standard Scala ExecutionContext, you can make this do whatever you want */
 object TestExecutionContext extends CacheExecutionContext {
-  protected val ec: ExecutionContextExecutor = ExecutionContext.Implicits.global
+  protected val ec: ExecutionContext = ExecutionContext.Implicits.global
   override def execute(runnable: Runnable): Unit = ec.execute(runnable)
 
   override def reportFailure(cause: Throwable): Unit = ec.reportFailure(cause)
